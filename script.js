c86fb342a5c3830a9198e3990089ed6e5e5e0874
Nik <finik433@yandex.ru>
2022-04-10 21:49:18 +0300

    add script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Add script');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '')
+
+const personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	ganre: [],
+	private: false
+}
+
+const a = prompt('Last film?', ''),
+			b = prompt('rating?', ''),
+			c = prompt('Last film?', ''),
+			d = prompt('rating?', '')
+
+personalMoviesDB[a] = b
+personalMoviesDB[c] = d
+console.log(personalMoviesDB)
